7ea33b1eb673f46542d533a7302a1da86c9b10d7
Alfo:3 <123606102+Alfo3@users.noreply.github.com>
2025-08-28 22:17:56 -0400

    version casi final

---
+++ LAB1_202373613-9_202304613-2.py
@@ -0,0 +1,171 @@
+import random
+
+#-------------------------------
+# DEFINICION DE CONVERSIONES
+#-------------------------------
+
+def decimal_binario(numero): #de decimal a binario
+    if numero == 0:
+        return "0"
+    bits = []
+    while numero > 0:
+        bits.append(str(numero % 2))
+        numero = numero // 2
+    return ''.join(reversed(bits)) #el join y '' es mandatorio, pq si no queda una lista asi
+                                    #['1', '0', '1']
+                                    # queda como texto y ademas queda como lista, y no como binario
+                                    #lo mismo pa todos los demas
+
+def decimal_octal(numero): #de decimal a octal
+    if numero == 0:
+        return "0"
+    bits = []
+    while numero > 0:
+        bits.append(str(numero % 8))
+        numero = numero // 8
+    return ''.join(reversed(bits)) #la misma explicacion q arriba >:(
+
+def decimal_hexa(numero):
+    if numero == 0:
+        return "0"
+    simbolos ='0123456789ABCDEF'
+    bits = []
+    while numero > 0:
+        bits.append(simbolos[numero % 16])
+        numero = numero // 16
+    return ''.join(reversed(bits))
+
+def binario_decimal(chain):
+    value = 0
+    for i, caracter in enumerate(reversed(str(chain))):
+        value += int(caracter) * (2**i)
+    return value
+
+def octal_decimal(chain):
+    value = 0
+    for i, caracter in enumerate(reversed(str(chain))):
+        value += int(caracter) *  (8**i)
+    return value
+
+def hexa_decimal(chain): #hexa solo funciona cuando 
+    value = 0
+    simbolos = '0123456789ABCDEF'
+    for i, caracter in enumerate(reversed(str(chain))): 
+        value += simbolos.index(caracter.upper()) * (16**i)
+    return value
+
+#-------------------------------
+# LOGICA MENU
+#-------------------------------
+def elegir_dificultad(nivel):
+    while True:
+        print()
+        print("Elige el sistema a hackear")
+        print("1) Firewall")
+        print("2) Servidor")
+        print("3) Memoria")
+        dificultad=(input("> "))
+        if(not(dificultad=='1' or dificultad=='2' or dificultad=='3')):
+            print("Error al elegir dificultad, por favor intente nuevamente\n")
+        else:
+            print()
+            if(dificultad=='1'):
+                print("Has escogido firewall!")
+                return dificultad
+            elif(dificultad=='2' and nivel>=3):
+                print("Has escogido servidor!")
+                return dificultad
+            elif(dificultad=='3' and nivel>=5):
+                print("Has escogido memoria!")
+                return dificultad
+            else:
+                print("No puedes escoger esta dificultad ya que eres nivel muy bajo, prueba hackeando sistemas inferiores.\n")
+
+def generar_decimal(dificultad):
+    match dificultad:
+        case '1':
+            decimal=random.randint(1,63)
+        case '2':
+            decimal=random.randint(1,511)
+        case '3':
+            decimal=random.randint(1,4095)
+    return decimal
+
+def generar_codigo(decimal, dificultad):
+    match dificultad:
+        case '1':
+            codigo=decimal_binario(decimal)
+        case '2':
+            codigo=decimal_octal(decimal)
+        case '3': 
+            codigo=decimal_hexa(decimal)
+    return codigo
+
+def descifrar_codigo(codigo, decimal, dificultad):
+    global nivel
+    respuesta=0
+    while respuesta != -1:
+        print("Debes convertir el código ", codigo, "a decimal.")
+        respuesta=int(input("Ingresa una respuesta. (-1 si deseas salir).\n>"))
+        if respuesta==decimal:
+            print("Felicidades, has descifrado correctamente el código.")
+            nivel = subir_nivel(dificultad, nivel)
+            return 
+        elif (respuesta != decimal and respuesta != -1):
+            print("Alto salame, intentalo nuevamente")
+        else:
+            print("Saliendo...")
+    return
+
+def subir_nivel(dificultad, nivel):
+    if (nivel<3 and dificultad=='1'):
+        nivel+=1
+    elif (nivel <5 and dificultad=='2'):
+        nivel+=1
+    elif (nivel <8 and dificultad=='3'):
+        nivel+=1
+    elif (nivel == 8):
+        print("Felicidades has llegado al nivel ANONYMOUS slay")
+    else:
+        print("Has alcanzado el limite de nivel para estas dificultades, avanza en los siguientes desafios para subir de nivel.")
+        return nivel
+    print("Has subido un nivel!")
+    return nivel
+
+#-------------------------------
+# MENU DE OPCIONES
+#-------------------------------
+
+def menu():
+    while True:
+        print("\nBienvenido al menu de opciones")
+        print("1) Generar Codigo 🐈")
+        print("2) Descifrar el Codigo 🤓")
+        print("3) Salir")
+        print("Tu nivel actual es: ", nivel)
+        option = input("Selecciona lo que quieres hacer: ").strip()
+
+        if option == '1':
+            dificultad=elegir_dificultad(nivel)
+            decimal=generar_decimal(dificultad)
+            codigo=generar_codigo(decimal, dificultad)
+            print ("El codigo generado es:", codigo)
+        elif option == '2':
+            print("\nHas elegido descifrar el código, buena suerte hacker.")
+            descifrar_codigo(codigo, decimal, dificultad)
+        elif option == '3':
+            break # aqui se sale del programa nomas, kaboom
+        else:
+            print("Opcion no valida, porfavor intenta denuevo\n")
+
+
+
+#-------------------------------
+# MAIN
+#-------------------------------
+nivel = 1
+menu()
+
+#Niveles arreglado yippie
+#Menu lowkey no se como hacerlo mas bonito, la vrd no soy bueno diseñandolols pero sabes q iwal se ve weno
+#pasable, yo creo q si
---
+++ README.txt
@@ -0,0 +1,71 @@
+-------------------------------------
+
+Alfonso Pavez 
+202373613-9
+paralelo:200
+
+-------------------------------------
+
+Hector Chanampe
+202304613-2
+paralelo:200
+
+-------------------------------------
+
+Algoritmos y funciones:
+
+El algoritmo general para decimal a binario, octal y hexadecimal, 
+es tomar un numero entero cualquiera con RANDOM, dividir sucesivamente por 2, 8 o 16
+guardar el residuo e invertir el orden para obtener el resultado esperado.
+
+Para el inverso, se recorre la cadena de izquierda a derecha, se multiplica por su potencia correspondiente
+y luego se suma todo el resultado.
+
+Para hexadecimal, en ambos casos se considera una variable "simbolos", la cual guarda los simbolos que 
+utliza.
+
+def decimal_binario(numero) -> convierte de decimal a binario, requiere de un numero entero
+def decimal_octal(numero): -> convierte de decimal a octal, requiere de un numero entero
+def decimal_hexa(numero): -> convierte de decimal a hexadecimal, requiere de un numero entero
+
+def binario_decimal(chain): -> convierte de binario a decimal, requiere una cadena de codigo binario
+def octal_decimal(chain): -> convierte de octal a decimal, requiere una cadena en codigo octal
+def hexa_decimal(chain): -> convierte de hexadecimal a decimal, requiere una cadena de codigo hexadecimal
+
+def elegir_dificultad(nivel): -> logica para elegir la dificultad del codigo, require el nivel actual
+                                 para verificar que se puede elegir la dificultad deseada, requiere un numero de nivel base (1)
+
+def generar_decimal(dificultad): -> utlizando la libreria RANDOM, se genera un numero en el rango
+                                    de dificultad permitido, requiere un input de dificultad (numero entero entre [1,3])
+
+def generar_codigo(decimal, dificultad): -> se convierte el decimal antes generado en el codigo correspondiente
+                                            requiere de un decimal entero, y de un input de dificultad
+
+def descifrar_codigo(codigo, decimal, dificultad): -> loop el cual pregunta por la respuesta del codigo generado
+                                                      si se ingresa mal, se avisa con un mensaje, de lo contrario
+                                                      avisa que fue descifrado correctamente y sube un nivel de dificultad
+                                                      require una cadena de codigo, un numero entero y un input de numero entero de dificultad
+
+def subir_nivel(dificultad, nivel): -> se sube el nivel actual en +1
+                                       requiere de un entero de dificultad y un entero de nivel
+
+
+def menu(): -> loop para el menu
+               no requiere entrada
+
+-------------------------------------
+
+Supuestos Utilizados:
+
+1. se asume que los hexadecimal pueden ser ingresados en ambos mayusuclas y minusculas
+2. se asume que todos los inputs seran validos, tal como los numeros enteros al momento de elegir en el menu
+
+-------------------------------------
+
+Metodo de ejecucion:
+1. ubicar en la consola el archivo "LAB1_202373613-9_202304613-2.py"
+2. ejecutar el comando "python3 LAB1_202373613-9_202304613-2.py" o "py LAB1_202373613-9_202304613-2.py"
+
+
+
+
